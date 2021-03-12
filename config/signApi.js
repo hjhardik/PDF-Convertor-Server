8b30ed022e7af418670ae9b660116e56e1455ab1
Hardik Joshi <hardikj7321@gmail.com>
2021-03-12 12:44:12 +0530

    updated

--- config/signApi.js
+++ config/signApi.js
@@ -1,5 +1,5 @@
 //SIGN API VARIABLES
-let redirectURL = "https://adobe-week-hardik.herokuapp.com/signfiles"
+let redirectURL = "https://transform-pdf.herokuapp.com/signfiles"
 let clientID = "CBJCHBCAABAAaAQ64--g1oDkEDTa0lQCVRydiTe4k7Wz"
 let clientSecret = "ufHzij20js0QpPAn60a7CyzsD_y5y9zN"
 
