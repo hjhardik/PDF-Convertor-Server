bf1f8173215e04e4c4071da003170af4fdcc095e
Hardik Joshi <hardikj7321@gmail.com>
2021-03-12 14:48:12 +0530

    updated signapi creden

--- config/signApi.js
+++ config/signApi.js
@@ -1,7 +1,7 @@
 //SIGN API VARIABLES
-let redirectURL = "https://transform-pdf.herokuapp.com/signfiles"
-let clientID = "CBJCHBCAABAAaAQ64--g1oDkEDTa0lQCVRydiTe4k7Wz"   
-let clientSecret = "ufHzij20js0QpPAn60a7CyzsD_y5y9zN"
+let redirectURL = "https://human-recruitment.herokuapp.com/signcontract"
+let clientID = "CBJCHBCAABAAN4Co_y_LKH5ZZrs7FZ1LHfibg_NMQ5Au"   
+let clientSecret = "ooryrja9VBwOoZuKv2nnyw0yd9qfubeG"
 
 module.exports = {
     redirectURL,
