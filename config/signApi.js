bc2b852e6c11b4cec76b56b3b637e4ee8f30c7d5
Hardik Joshi <hardikj7321@gmail.com>
2021-04-11 13:45:53 +0530

    sign api cred changed

--- config/signApi.js
+++ config/signApi.js
@@ -1,5 +1,5 @@
 //SIGN API VARIABLES
-let redirectURL = "https://transform-pdf.herokuapp.com/signfiles"
+let redirectURL = "https://pdfconvertorapp.netlify.app/signfiles"
 let clientID = "CBJCHBCAABAAN4Co_y_LKH5ZZrs7FZ1LHfibg_NMQ5Au"   
 let clientSecret = "ooryrja9VBwOoZuKv2nnyw0yd9qfubeG"
 
